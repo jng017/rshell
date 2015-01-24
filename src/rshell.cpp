3f5ee61ab6adab90d5abd462f1ba78d28d22521e
jng017 <jng017@ucr.edu>
2015-01-23 20:04:12 -0800

    Added functionality for && cases. Still working on ||.

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <stdio.h>
 #include <errno.h>
@@ -13,13 +12,20 @@ using namespace std;
 
 //To Do List:
 //Create Connectors && || and ; and provide functionality for them.
-
-void exit(int a)
+int exit(int a)
 {
-	return;
+	if(a == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+
 }
 
-void readcommand(char *firstparse[])
+int readcommand(char *firstparse[])
 {
 	int pid = fork();
 	if(pid == -1)
@@ -30,46 +36,16 @@ void readcommand(char *firstparse[])
 	else if(pid == 0)
 	{
 		if(-1 == execvp(firstparse[0], firstparse))
-		{
 			perror("There was an error running execvp.");
-		}
-		exit(1);
+		return exit(-1);
 	}
 	else if(pid > 0)
 	{
 		if(-1 == wait(0))
 			perror("There was an error with wait. ");
-		exit(1);
+		return exit(0);
 	}
-}
-
-void readcommandand(char *firstparse[], char *secondparse[])
-{
-	int pid = fork();
-	if(pid == -1)
-	{
-		perror("Fork error.");
-		exit(1);
-	}
-	else if(pid == 0)
-	{
-		if(-1 == execvp(firstparse[0], firstparse))
-		{
-			perror("And case failed. Second parse will not run.");
-		}
-		else
-		{
-			if(-1 == execvp(secondparse[0], secondparse))
-				perror("Second and case failed.");
-		}
-		exit(1);
-	}
-	else if(pid > 1)
-	{
-		if(-1 == wait(0))
-			perror("There was an error using wait.");
-			exit(1);
-	}
+	return exit(0);
 }
 
 void commandprompt()
@@ -78,51 +54,207 @@ void commandprompt()
 	
 	char userinput[50000];
 	cin.getline(userinput, 50000, '\n');
-	char *token;
-	token = strtok(userinput, " ");
+	char *token, *save_1, *token2, *save_2;
+	char *tokenand1, *tokenand2, *save_and_1, *save_and_2, *save_and_3;
+	char *tokenor1, *tokenor2, *save_or_1, *save_or_2, *save_or_3;
+	bool nospaces = true;
+	token = strtok_r(userinput, ";", &save_1);
 	
-	int i = 0;
-	bool noconnectors = true;
 	char *parsedline[50];
+	int i, k, andcounter, andcounter2, orcounter, orcounter2 = 0;
+	bool noextras = true;
+	bool andcase = false;
+	bool orcase = false;
 	while(token != NULL)
 	{
-		string s = token;
-		if(token[0] == '#')
+		k = i;
+		nospaces = true;
+		andcase = false;
+		orcase = false;
+		cout << "Current Tokenized String: " << token << " i: " << i << " k: " << k << endl;
+		for(int j = 0; token[j] != '\0'; ++j)
 		{
-			break;
+			if(token[j] == '#')
+			{
+				noextras = false;
+				cout << "Experiencing comment case. Breaking from loop." << endl;
+				break;
+			}
+			else if(token[j] == '&' && token[j+1] == '&')
+			{
+				noextras = false;
+				andcase = true;
+				cout << "Experiencing and case. Executing protocol." << endl;
+			}
+			else if(token[j] == '|' && token[j+1] == '|')
+			{
+				noextras = false;
+				orcase = true;
+				cout << "Experiencing or case. Executing protocol." << endl;
+			}
+			else
+			{
+				continue;
+			}
 		}
-		//else if(token[0] == '&' && token[1] == '&')
-		//{
-		//	noconnectors = false;
-		//	char* andsituation[50];
-	//		int j = 0;
-	//		while(token != NULL)
-	//		{
-	//			token = strtok(NULL, " " );
-	//			andsituation[j] = token;
-	//			j++;
-	//		}
-	//		readcommandand(parsedline, andsituation);
-	//		break;
-	//	}
-		//else if(token[0] == '|' && token[1] == '|')
-		//{
+		if(andcase)
+		{
+			tokenand1 = strtok_r(token, "&", &save_and_1);
+			tokenand2 = strtok_r(tokenand1, " ", &save_and_2);
+			while(tokenand2 != NULL)
+			{
+				nospaces = false;
+				parsedline[i] = tokenand2;
+				tokenand2 = strtok_r(NULL, " ", &save_and_2);
+				i++;
+				andcounter++;
+			}	
+			if(nospaces)
+			{
+				parsedline[i] = tokenand2;
+				i++;
+				andcounter++;
+			}
+			char* firstparse_and[50];
+			for(int a = k; a < (k+andcounter); ++a)
+			{
+				firstparse_and[a-k] = parsedline[a];
+			}
+			if(orcase)
+			{
+				tokenand1 = strtok_r(NULL, "&", &save_and_1);
+				tokenand1 = strtok_r(NULL, "|", &save_and_1);
+			}
+			if(!orcase)
+			{
+				tokenand1 = strtok_r(NULL, "&", &save_and_1);
+			}
 
-		//}
-	//	else if(s[s.size()-1] == ';')
-	//	{
-	//		
-	//	}
-//		else
-//		{
-			parsedline[i] = token;
-			token = strtok(NULL, " ");
-			i++;
-//		}
-	}
-	if(noconnectors)
-	{
-		readcommand(parsedline);
+			tokenand2 = strtok_r(tokenand1, " ", &save_and_3);
+			nospaces = true;
+			while(tokenand2 != NULL)
+			{
+				nospaces = false;
+				parsedline[i] = tokenand2;
+				tokenand2 = strtok_r(NULL, " ", &save_and_3);
+				i++;
+				andcounter2++;
+			}
+			if(nospaces)
+			{
+				parsedline[i] = tokenand2;
+				i++;
+				andcounter2++;
+			}
+			char* secondparse_and[50];
+			for(int b = (k+andcounter) ; b < (k+andcounter+andcounter2); ++b)
+			{
+				secondparse_and[b-(k+andcounter)] = parsedline[b];
+			}
+
+			if(-1 == readcommand(firstparse_and))
+			{
+				break;
+			}
+			else
+			{
+				readcommand(secondparse_and);
+			}
+
+		}
+
+		if(orcase)
+		{
+			tokenor1 = strtok_r(token, "|", &save_or_1);
+			tokenor2 = strtok_r(tokenor1, " ", &save_or_2);
+			while(tokenor2 != NULL)
+			{
+				nospaces = false;
+				parsedline[i] = tokenor2;
+				tokenor2 = strtok_r(NULL, " ", &save_or_2);
+				i++;
+				orcounter++;
+			}	
+			if(nospaces)
+			{
+				parsedline[i] = tokenor2;
+				i++;
+				orcounter++;
+			}
+			char* firstparse_or[50];
+			for(int f = k; f < (k+orcounter); ++f)
+			{
+				firstparse_or[f-k] = parsedline[f];
+			}
+			if(andcase)
+			{
+				tokenor1 = strtok_r(NULL, "|", &save_or_1);
+				tokenor1 = strtok_r(NULL, "&", &save_or_1);
+			}
+			if(!andcase)
+			{
+				tokenor1 = strtok_r(NULL, "|", &save_or_1);
+			}
+
+			tokenor2 = strtok_r(tokenor1, " ", &save_or_3);
+			nospaces = true;
+			while(tokenor2 != NULL)
+			{
+				nospaces = false;
+				parsedline[i] = tokenor2;
+				tokenor2 = strtok_r(NULL, " ", &save_or_3);
+				i++;
+				orcounter2++;
+			}
+			if(nospaces)
+			{
+				parsedline[i] = tokenor2;
+				i++;
+				orcounter2++;
+			}
+			char* secondparse_or[50];
+			cout << "Second Array(Or): " ;
+			for(int p = (k+orcounter) ; p < (k+orcounter+orcounter2); ++p)
+			{
+				secondparse_or[p-(k+orcounter)] = parsedline[p];
+				cout << parsedline[p] << " at " << (p-(k+orcounter)) << " ";
+			}
+
+			if(-1 == readcommand(firstparse_or))
+			{
+				if(-1 != readcommand(secondparse_or))
+				{
+					break;
+				}
+			}
+		}
+		
+		else
+		{
+			token2 = strtok_r(token, " ", &save_2);
+			while(token2 != NULL)
+			{
+				nospaces = false;
+				parsedline[i] = token2;
+				token2 = strtok_r(NULL, " ", &save_2);
+				i++;
+			}
+			if(nospaces)
+			{
+				parsedline[i] = token2;
+				i++;
+			}
+			if(noextras)
+			{
+				char* parsedlinecopy[50];
+				for(int a = k; a <= i; ++a)
+				{
+					parsedlinecopy[a-k] = parsedline[a];
+				}
+				readcommand(parsedlinecopy);
+			}
+			token = strtok_r(NULL, ";", &save_1);
+		}
 	}
 }
 
@@ -130,7 +262,7 @@ void commandprompt()
 int main(int argc, char** argv)
 {
 	int count = 0;
-	while(count < 3)
+	while(count < 5)
 	{
 		
 		commandprompt();
